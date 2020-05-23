ef17b717bd9d2b812ada2c258e32c26835a20a35
Atikur Rahman <mdatikur469@gmail.com>
2020-05-24 00:01:03 +0600

    Build Ready/Atik

---
+++ Assets/Script/MainManue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManue : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Ok");
+        SceneManager.LoadScene ("GameScene");
+        //Debug.Log("Pressed");
+    }
+
+    public void update()
+    {
+        Debug.Log("Working");
+    }
+}
